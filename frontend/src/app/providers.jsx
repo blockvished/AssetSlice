5b343511da17b7a92b54da54434d390c74c3c7a7
dhanush-devx <dhanush.devx@gmail.com>
2025-08-16 16:34:28 +0530

    add sapoli

--- frontend/src/app/providers.jsx
+++ frontend/src/app/providers.jsx
@@ -3,13 +3,13 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { avalanche, avalancheFuji, arbitrum, mainnet } from 'wagmi/chains';
+import { avalanche, avalancheFuji, sepolia, arbitrum, mainnet } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const config = getDefaultConfig({
   appName: 'My Next.js 15 App',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID, // from WalletConnect Cloud
-  chains: [avalanche, avalancheFuji, mainnet, arbitrum],
+  chains: [avalanche, avalancheFuji, sepolia, mainnet, arbitrum],
   ssr: true, // important for Next.js App Router
 });
 
