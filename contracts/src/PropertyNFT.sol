9c573489dc6e4bbba6e8c717f35238d4a58e4789
blockvished <jcmvishael@gmail.com>
2025-08-16 08:07:37 +0000

    Property nft update

--- contracts/src/PropertyNFT.sol
+++ contracts/src/PropertyNFT.sol
@@ -7,11 +7,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract PropertyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
 
-    constructor() ERC721("PropertyNFT", "PROP") {}
+    struct PropertyMetadata {
+        string name;
+        string description;
+        string image; // e.g. ipfs://CID/image.png
+    }
 
-    function mintProperty(address to) external onlyOwner returns (uint256) {
+    mapping(uint256 => PropertyMetadata) private _properties;
+
+    constructor() ERC721("PropertyNFT", "PROP") Ownable(msg.sender) {}
+
+    function mintProperty(
+        address to,
+        string memory uriName,
+        string memory uriDescription,
+        string memory uriImage
+    ) external onlyOwner returns (uint256) {
         uint256 tokenId = nextTokenId++;
         _mint(to, tokenId);
+
+        _properties[tokenId] = PropertyMetadata({
+            name: uriName,
+            description: uriDescription,
+            image: uriImage
+        });
+
         return tokenId;
+    }
+
+    /// @notice Returns on-chain metadata (not the JSON URI, just struct fields)
+    function getProperty(uint256 tokenId) external view returns (PropertyMetadata memory) {
+        require(_ownerOf(tokenId) != address(0), "ERC721: invalid token ID");
+        return _properties[tokenId];
     }
 }
