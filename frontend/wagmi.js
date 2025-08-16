a565ac5a2a16703d46c375c90eaaaa671ced1c03
blockvished <jcmvishael@gmail.com>
2025-08-16 12:16:17 +0000

    added more chains

--- frontend/wagmi.js
+++ frontend/wagmi.js
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { avalanche, arbitrum, mainnet } from 'wagmi/chains';
+import { avalanche, arbitrum, mainnet, avalancheFuji, sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'My Next.js 15 App',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID, // from WalletConnect Cloud
-  chains: [avalanche, mainnet, arbitrum],
+  chains: [avalanche, avalancheFuji, sepolia, mainnet, arbitrum],
   ssr: true, // important for Next.js App Router
 });
