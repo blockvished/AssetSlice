3f86ff736f306352fe2ef012efc31843571ee4c2
dhanush-devx <dhanush.devx@gmail.com>
2025-08-16 18:03:48 +0530

    add fractionalize navbar

--- frontend/src/app/fractionalize/page.jsx
+++ frontend/src/app/fractionalize/page.jsx
@@ -1,7 +1,10 @@
+
 export default function Fractionalize() {
   return (
-    <div>
-        fractionalize
+    <div className="min-h-screen bg-gray-50 py-10">
+      <h1 className="text-2xl font-bold text-center mb-6">Fractionalize NFT</h1>
+      <FractionalizeForm />
     </div>
   );
 }
+import FractionalizeForm from "../../components/fractionalizeform";
